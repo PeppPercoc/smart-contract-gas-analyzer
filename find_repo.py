f88d8e425cd3d82449b73e14c5983b5eba5ef310
Paolo <paolo.martino1234@gmail.com>
2025-10-05 18:40:31 +0200

    Improving url in requesthttp

--- find_repo.py
+++ find_repo.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,24 +7,30 @@ GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 if not GITHUB_TOKEN:
     raise ValueError("Github Token not found! Check file .env")
 
+HEADERS = {"Authorization": f"token {GITHUB_TOKEN}"}
+
 def search_repository(query):
     repos=[]
-    per_page=50
-    max_pages=20 #20*50=1000 numero massimo richieste
+    per_page=1
+    max_pages=1 #20*50=1000 numero massimo richieste
     for page in range(1,max_pages+1):
         url = f"https://api.github.com/search/repositories?q={query}&sort=stars&order=desc&per_page={per_page}&page={page}"
-        print("l'url è ")
-        print(url)
+        response = requests.get(url, headers=HEADERS)
+        if response.status_code != 200:
+            print(f"Errore {response.status_code}: {response.text}")
+            break
+        repos.extend(response.json().get("items", []))
+    return repos
 
 
 def main():
-    print(GITHUB_TOKEN)
     star="10"
     language="Solidity"
     created="2019-01-01"
     archived="false"
     query="stars:>"+star+" language:"+language+" created:>"+created+" archived:"+archived
     repos=search_repository(query)
+    print(repos)
 
 if __name__ == "__main__":
     main()
